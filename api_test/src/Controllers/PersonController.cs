e20dd87d88b1382cea26f7d259558ae2f443630a
ADavid <davidantoniodavi@gmail.com>
2024-02-15 15:57:54 -0300

    Created the class Person Controller class

---
+++ api_test/src/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase{
+
+    [HttpGet]
+
+    public string Hello(){
+        return "Olá, gatão!";
+    }
+
+}
