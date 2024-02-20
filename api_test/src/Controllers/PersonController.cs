a1da60534821b3e08d6c861fc6ac5adea5c0bdd8
ADavid <davidantoniodavi@gmail.com>
2024-02-20 13:13:19 -0300

    refactored

--- api_test/src/Controllers/PersonController.cs
+++ api_test/src/Controllers/PersonController.cs
@@ -16,7 +16,7 @@ public class PersonController : ControllerBase{
         Person pessoa = new Person("Antonio", 22, "08608986523", true);
         pessoa.Contractos.Add(new Contract("a9sdjakldjald", 20000.00));
         pessoa.Contractos.Add(new Contract("9asdaus0asudu0au0du1", 90000.00));
-
+        
         return pessoa;
     }
 
